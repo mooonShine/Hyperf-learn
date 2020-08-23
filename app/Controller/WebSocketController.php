9a8d312fea5533988539bfddf923bb732bde2a72
mooonShine <969487286@qq.com>
2020-08-23 16:21:20 +0800

    测试消息发送

--- app/Controller/WebSocketController.php
+++ app/Controller/WebSocketController.php
@@ -15,7 +15,13 @@ class WebSocketController implements OnMessageInterface, OnOpenInterface, OnClos
 {
     public function onMessage($server, Frame $frame): void
     {
-        $server->push($frame->fd, 'Recv: ' . $frame->data);
+      //  $server->push($frame->fd, 'Recv: ' . $frame->data);
+        $recvData = json_decode($frame->data);
+        if(!is_object($recvData)) {
+            $this->checkData($frame->data,$frame->fd);
+        } else {
+            $this->sendData($server,$frame->data,$frame->fd);
+        }
     }
 
     public function onClose($server, int $fd, int $reactorId): void
@@ -26,5 +32,67 @@ class WebSocketController implements OnMessageInterface, OnOpenInterface, OnClos
     public function onOpen($server, Request $request): void
     {
         $server->push($request->fd, 'Opened');
+    }
+
+    /**
+     * 校验数据
+     * @param $string
+     * @param $fd
+     * @return string
+     */
+    function checkData ($string, $fd) {
+        if (!is_string($string)) {
+          //  $this->logger->error('字符串类型错误');
+            return '字符串类型错误';
+        }
+
+        $strArray = explode('_',$string);
+        $shopIds = json_decode($string[1],true);
+
+        if(!is_array($shopIds) || empty($shopIds)) {
+        //    $this->logger->error('参数错误');
+            return '参数有误';
+        }
+
+        echo "全部映射成功";
+
+   }
+
+
+    /**
+     * 发送消息到 PC 端
+     */
+    public function sendData($server,$sendData,$fd) {
+        $recvData = json_encode($sendData,true);
+        $uid = $recvData['uid'];
+        $data = $recvData['data'];
+
+       // $fdsArr = $this->redis->sMembers('jiayouwa:websocket:voiceSet_'.$uid);
+
+        echo 'voiceSet_'.$uid;
+
+        $data = [
+            'result' => true,
+            'code'=>0,
+            'msg'=>'操作成功',
+            'data'=>$data,
+        ];
+        $server->push(intval($fd),json_encode($data));
+
+
+//        if(count($fdsArr)) {
+//            foreach ($fdsArr as $key=>$value) {
+//                try {
+//                    $server->push(intval($value),json_encode($data));
+//                    echo "线程：$fd 向线程 $value 发送信息\\n";
+//                } catch (\Throwable $e) {
+//                    // 增加 重试次数
+//                    $this->service->push($recvData,1);
+//                    // 把数据删除
+//                  //  $this->redis->sRem('jiayouwa:websocket:voiceSet_'.$uid,$value);
+//                    continue;
+//                }
+//            }
+//        }
     }
 }
